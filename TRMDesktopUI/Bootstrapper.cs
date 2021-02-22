4148d6660ded70b480375779ebb32f6165af75a5
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-02-23 00:31:54 +0100

    Add dependency injection
    Added dependency injection containers via caliburn micro

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -11,14 +11,56 @@ namespace TRMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
         }
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            // Window manager: bringing windows in and out
+            // one instance of window manager is needed
+            // Event aggregator: raising events and put pieces listening to it.
+            // we are going to connect to events through one aggregator.
+            // we register the WindowManager class as its Interface.
+            //             the EventAggretator as its Interface. We are going to ask for the interfaces.
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Reflections are performance-heavy but this is a one-time use of it, so its a good use. GetType is a reflection
+            // Get every type of the current assembly where a type is a class and its name ends with "ViewModel"
+            // Create a list of the ViewModels and go through all of them and register them to the container instance
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }

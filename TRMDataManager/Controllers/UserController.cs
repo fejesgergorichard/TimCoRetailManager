bb8a14badb1bd14363717350c2b0fb53dc30c389
fejesgergorichard <50084782+fejesgergorichard@users.noreply.github.com>
2021-02-28 19:11:38 +0100

    Add another API Endpoint to tht UserController
    Added another route to a UserModel returning GET method for practice.

--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Web;
 using System.Web;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
@@ -10,11 +11,22 @@ using TRMDataManager.Library.Models;
 namespace TRMDataManager.Controllers
 {
     [Authorize]
+    [RoutePrefix("api/User")]
     public class UserController : ApiController
     {
-        public UserModel GetById()
+        // GET api/user
+        public UserModel Get()
         {
             string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId).First();
+        }
+
+        // GET api/user/5
+        [Route("{userId}")]
+        public UserModel Get(string userId)
+        {
             UserData data = new UserData();
 
             return data.GetUserById(userId).First();
